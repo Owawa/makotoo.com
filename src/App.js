2e1e342e8285b4e327c7d5ff6e8bd719f0b26d72
Owawa <makotograffitti@gmail.com>
2020-01-07 06:16:09 +0900

    Add state change function, hover behavior of UIs.

--- src/App.js
+++ src/App.js
@@ -1,49 +1,84 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import PropTypes from 'prop-types';
 
-const Header = () => {
+const Header = (props) => {
   return (
       <header>
-          <div className="header-logo">makotoo.com</div>
-          <div className="header-list">
-            <ul>
-              <li>あそぶ</li>
-              <li>makotooについて</li>
-            </ul>
-          </div>
+        <div className="header-logo" onClick={props.handleTopLinkPushed}>makotoo.com</div>
+          <nav>
+            <span onClick={props.handlePlayLinkPushed}>あそぶ</span>
+            <span>makotooについて</span>
+          </nav>
       </header>
   )
 }
 
-const Footer = () => {
+Header.propTypes = {
+  handlePlayLinkPushed : PropTypes.func,
+  handleTopLinkPushed : PropTypes.func
+}
+
+const Footer = (props) => {
   return (
       <footer>
-        <div className="footer-logo">makotoo.com</div>
-        <div className="footer-list">
-          <ul>
-            <li>あそぶ</li>
-            <li>makotooについて</li>
-          </ul>
-        </div>
+        <div className="footer-logo" onClick={props.handleTopLinkPushed}>makotoo.com</div>
+        <ul className="footer-list">
+          <li onClick={props.handlePlayLinkPushed}>あそぶ</li>
+          <li>makotooについて</li>
+        </ul>
       </footer>
   )
 }
 
+Footer.propTypes = {
+  handlePlayLinkPushed : PropTypes.func
+}
 
-function App() {
-  return (
-    <React.Fragment>
-      <Header />
-      <main>
-        <p>あああああ</p>
-        <p>あああああ</p>
-        <p>あああああ</p>
-        <p>あああああ</p>
-        <p>ABCD</p>
-      </main>
-      <Footer />
-    </React.Fragment>
-  );
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { currentContent : "TOP" };
+  }
+
+  playLinkPushed () {
+    this.setState({ currentContent : "PLAY" });
+  }
+
+  topLinkPushed () {
+    this.setState({ currentContent : "TOP" });
+  }
+
+  render() {
+    let Main = null;
+    if(this.state.currentContent === "TOP") {
+      Main = <main>
+               <p>あああああ</p>
+               <p>あああああ</p>
+               <p>あああああ</p>
+               <p>あああああ</p>
+               <p>ABCD</p>
+             </main>;
+    } else if(this.state.currentContent === "PLAY") {
+      Main = <main>play</main>;
+    }
+
+    return (
+      <React.Fragment>
+      <Header
+        handlePlayLinkPushed={this.playLinkPushed.bind(this)}
+        handleTopLinkPushed={this.topLinkPushed.bind(this)}
+        />
+      {Main}
+      <Footer
+        handlePlayLinkPushed={this.playLinkPushed.bind(this)}
+        handleTopLinkPushed={this.topLinkPushed.bind(this)}
+      />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
